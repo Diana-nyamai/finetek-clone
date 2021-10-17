e57782fe4fa8a5bf9065e7f5efbfefb8fbed2399
Diana-nyamai <dnyamai.dn@gmail.com>
2021-10-17 08:48:47 +0300

    update

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  // collapsable header
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+  
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      } 
+    });
+  }
